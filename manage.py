59c2fad3c558bf5f03f200ef15ee59fb4f0fac6a
AokoMercyline <aokomercyline34@gmail.com>
2020-09-22 19:43:36 +0300

    manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 # from app import app
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Comment,Pitch,PitchCategory
+from app.models import User,Role,Comment,Pitch,PitchCategory
 from  flask_migrate import Migrate, MigrateCommand
 
 app = create_app('test')
