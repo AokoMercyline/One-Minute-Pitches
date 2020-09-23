a5b441ff732784175dee93b30de2e7d03f5ed3f7
AokoMercyline <aokomercyline34@gmail.com>
2020-09-23 12:48:07 +0300

    start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 export SECRET_KEY='+D\xdf\x11\xe7\x06Fq?\x85\xbc\xc3'
 
 export MAIL_USERNAME='aokomercyline34@gmail.com'
-export MAIL_PASSWORD='mao2019abc'
+export MAIL_PASSWORD='0708434156mercy'
 # python3.6 manage.py server
 
 python3.8 manage.py server
